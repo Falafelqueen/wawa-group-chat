3a371b7c95d235eaf68d22a267c59dd00213b648
Lucia Harcegova <fakeluciamail@gmail.com>
2023-11-10 11:48:39 +0100

    Fixed bug

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,7 +8,7 @@ class Message < ApplicationRecord
 
   def update_chatroom
     # Append message to chatroom
-    broadcast_append_to "chatroom", target: "messages", partial: "messages/message", locals: { message: self }
+    broadcast_append_to "chatroom", target: "messages_#{chatroom.id}", partial: "messages/message", locals: { message: self }
     # broadcast_append_to "chatroom",target: "messages", partial: "messages/message", locals: { message: self, sender: false }
     broadcast_replace_to "chatrooms", partial: "chatrooms/chatroom", target: "chatroom_#{self.chatroom.id}", locals: {chatroom: self.chatroom}
   end
